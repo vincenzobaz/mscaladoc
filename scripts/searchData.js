0dbeb82403db5f5953f9ea77e7dd856879ba6ad7
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2021-12-14 12:30:12 +0000

    Deploying to gh-pages from @ vincenzobaz/mscaladoc@1e3785b4ee23c521738fa6218ea6f75ff120d13e 🚀

---
+++ scripts/searchData.js
@@ -0,0 +1,5 @@
+pages = [{"l":"docs/index.html","n":"docs","t":"docs","d":"","k":"static"},
+{"l":"docs/introduction.html","n":"introduction","t":"introduction","d":"","k":"static"},
+{"l":"index.html","n":"API","t":"API","d":"","k":"static"},
+{"l":"_empty_.html","n":"<empty>","t":"package <empty>","d":"<empty>","k":"package"},
+{"l":"_empty_.html","n":"superFunction","t":"def superFunction(x: Int): String","d":"<empty>","k":"def"}];
